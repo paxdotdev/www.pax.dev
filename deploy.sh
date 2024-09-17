6db02ccf81f4df947f03e443cdd95ff6cb2eb89e
Zack Brown <zack@pax.dev>
2024-09-16 18:02:54 -0700

    update deploy script and index.html

--- deploy.sh
+++ deploy.sh
@@ -8,5 +8,5 @@ set -e
 cp -r .pax/build/release/web/assets ./public/assets
 cp .pax/build/release/web/pax-* ./public/
 
-aws --profile=pax s3 sync --exclude ".git/*" --acl=public-read ./public s3://www.pax.dev/ 
+aws --profile=pax s3 sync --exclude ".git/*" --acl=public-read ./public/ s3://www.pax.dev/
 aws --profile=pax cloudfront create-invalidation --distribution-id=EYKZZ3KH242XU --paths "/*"
