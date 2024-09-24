c7afe4ddd78b5e7c15d2055fbb5738cf31938b7f
Zack Brown <zack@pax.dev>
2024-09-24 16:10:24 -0700

    wip

--- src/lib.rs
+++ src/lib.rs
@@ -14,13 +14,13 @@ pub use calculator::Calculator;
 #[main]
 #[file("lib.pax")]
 #[has_helpers]
-pub struct Example {
+pub struct PaxDotDev {
     pub responsive_stacker_direction: Property<StackerDirection>,
     pub is_mobile: Property<bool>,
 }
 
 
-impl Example {
+impl PaxDotDev {
 
     pub fn handle_mount(&mut self, ctx: &NodeContext) {
         
@@ -32,12 +32,20 @@ impl Example {
 }
 
 #[helpers]
-impl Example {
+impl PaxDotDev {
     pub fn get_stacker_direction(is_mobile: bool) -> StackerDirection {
         if is_mobile {
             StackerDirection::Vertical
         } else {
             StackerDirection::Horizontal
+        }
+    }
+
+    pub fn get_responsive_text_size(is_mobile: bool) -> Size {
+        if is_mobile {
+            Size::Pixels(16.into())
+        } else {
+            Size::Pixels(24.into())
         }
     }
 }
